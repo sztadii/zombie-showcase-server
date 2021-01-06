fd29a7346a0b0c1d77fbd3c8ab3749054e497a6f
Krystian Sztadhaus <sztadii@gmail.com>
2021-01-06 21:22:39 +0400

    During zombie deletion remove related zombie items

--- src/zombies/zombies.module.ts
+++ src/zombies/zombies.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common'
 import { ZombiesController } from './zombies.controller'
 import { ZombiesService } from './services/zombies.service'
+import { ZombiesItemsService } from '../zombies-items/services/zombies-items.service'
 
 @Module({
   controllers: [ZombiesController],
-  providers: [ZombiesService],
-  exports: [ZombiesService]
+  providers: [ZombiesService, ZombiesItemsService],
+  exports: [ZombiesService],
+  imports: [ZombiesItemsService]
 })
 export class ZombiesModule {}
